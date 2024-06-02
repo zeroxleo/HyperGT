a5ed6f61e7481e93ebb718848a8365e2290b6907
Zexi Liu <131458808+zeroxleo@users.noreply.github.com>
2024-06-03 00:59:08 +0800

    Add files via upload
    add environments set up scripts.

---
+++ install.sh
@@ -0,0 +1,8 @@
+pip install tqdm==4.64.1
+pip install scipy==1.9.1
+pip install numpy==1.23.4
+pip install torch==1.9.1+cu111 torchaudio -f https://download.pytorch.org/whl/cu111/torch_stable.html
+pip install torch-scatter==2.0.9 -f https://pytorch-geometric.com/whl/torch-1.9.1+cu111.html
+pip install torch-sparse==0.6.12 -f https://pytorch-geometric.com/whl/torch-1.9.1+cu111.html
+pip install torch-geometric==1.7.2
+
