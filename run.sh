7e697f9476727b10739470e8bfca877834cc894c
Zexi Liu <131458808+zeroxleo@users.noreply.github.com>
2024-11-24 00:14:30 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,8 +11,8 @@ python main.py --dataset senate-committees-100 --rand_split --metric acc --metho
     --hefeat mean --feature_noise 1.0
 
 python main.py --dataset walmart-trips-100 --rand_split --metric acc --method hypergt --lr 1e-3 \
-    --weight_decay 5e-2 --num_layers 2 --hidden_channels 64 --num_heads 1 --rb_order 0 --rb_trans sigmoid \
-    --lamda 1.0 --M 30 --K 10  --use_bn --use_residual --use_gumbel --runs 10 --epochs 500 --dropout 0.3 \
+    --weight_decay 0 --num_layers 3 --hidden_channels 128 --num_heads 1 --rb_order 0 --rb_trans sigmoid \
+    --lamda 1.0 --M 30 --K 10  --use_bn --use_residual --use_gumbel --runs 10 --epochs 500 --dropout 0.0 \
     --device 3 --data_dir ./data/pyg_data/hypergraph_dataset_updated --sloss --pe HEPEHtEPE \
     --hefeat mean --feature_noise 1.0   
 
